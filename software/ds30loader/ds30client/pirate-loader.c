9738a1299ae160a1a2ed76846f2150c7566fbe43
Your Name <you@example.com>
2014-06-26 15:08:24 +0200

    fixed jumps - hopefully

--- software/ds30loader/ds30client/pirate-loader.c
+++ software/ds30loader/ds30client/pirate-loader.c
@@ -546,11 +546,11 @@ void fixJumps(uint8* bin_buff, uint8* pages_used)
 	bin_buff[3] = 0x00;	
 	bin_buff[4] = ( (iBLAddress & 0x7F0000) >> 16 );
 	bin_buff[5] = 0x00; */
-	/* ORG 0x000 GOTO bootldr PIC18 mnemonic */
-	bin_buff[0] = 0xe0;
-	bin_buff[1] = ((iBLAddress & 0x000000F ) >>  1 );
-	bin_buff[2] = ((iBLAddress & 0xF000000 ) >> 21 ) | 0xF0;
-	bin_buff[3] = ((iBLAddress & 0x0FF0000 ) >> 17 );
+	/* ORG 0x0000 GOTO bootldr PIC18 mnemonic */
+	bin_buff[0] = ((iBLAddress & 0x00001FE ) >>  1 );
+	bin_buff[2] = 0xEF;
+	bin_buff[2] = ((iBLAddress & 0x001FE00 ) >>  9 );
+	bin_buff[3] = ((iBLAddress & 0x1FE0000 ) >> 17 ) | 0xF0;
 }
 
 /* non-firmware functions */
