81d86387f6fe4fae375d44cb42487e504bd24029
Your Name <you@example.com>
2014-07-02 20:45:21 +0200

    test simple circular

---
+++ test_buffer/buffer.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+#define SERIAL_BUFFER_SIZE      16
+#define SERIAL_BUFFER_SIZE_MASK (SERIAL_BUFFER_SIZE -1)
+
+struct serial_buffer {
+    unsigned char head;
+    unsigned char tail;
+    unsigned char data[SERIAL_BUFFER_SIZE];
+};
+
+void debug_print_fifo( struct serial_buffer *fifo) {
+    int head;
+    int tail;
+    head = fifo->head;
+    tail = fifo->tail;
+    printf("\nhead: %2d tail %2d\n",head,tail);
+}
+
+char fifo_putchar(struct serial_buffer *fifo) {
+    unsigned char tail=fifo->tail;
+    printf("printing chars\n");
+    if (fifo->head != tail) {
+	tail++;
+	tail&=SERIAL_BUFFER_SIZE_MASK;
+        if (putchar(fifo->data[tail])) {
+            tail&=SERIAL_BUFFER_SIZE_MASK;      /* wrap around if neededd */
+            fifo->tail=tail;
+    	    debug_print_fifo(fifo);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/* print into circular buffer */
+/* TODO: check if local var head speed up function */
+
+char print_fifo(const unsigned char *s, struct serial_buffer *fifo) {
+    unsigned char head=fifo->head;
+    char c;
+    while ( ( c = *s++ ) ) {
+        head++;
+        head&=SERIAL_BUFFER_SIZE_MASK;          /* wrap around if neededd */
+        printf("pf head: %2d tail %2d\n",head,fifo->tail);
+        if (head != fifo->tail) {               /* space left ? */
+            fifo->data[head]=c;
+        } else {
+            return -1;
+        }
+    }
+    fifo->head=head;                            /* only store new pointer if all is OK */
+    return 1;
+}
+
+int main( int argc,const char* argv[]) {
+    struct serial_buffer fifo;
+    char ret;
+    fifo.head=0;
+    fifo.tail=0;
+    ret=print_fifo("Hello world !\n",&fifo);
+    // debug_print_fifo(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=print_fifo("Der Hund.\n",&fifo);
+    // debug_print_fifo(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    ret=fifo_putchar(&fifo);
+    printf("ret: %d\n",ret);
+    return 0;
+}
