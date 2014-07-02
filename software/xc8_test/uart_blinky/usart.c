45fcbb4b2ee559b857c5b985ea836e0ac7c12483
Your Name <you@example.com>
2014-07-02 17:58:26 +0200

    play with USART init

--- software/xc8_test/uart_blinky/usart.c
+++ software/xc8_test/uart_blinky/usart.c
@@ -11,20 +11,20 @@
 
 void init_usart (void) {
     // USART configuration
-    TXSTAbits.TX9  = 0;		// 8-bit transmission
-    TXSTAbits.TXEN = 1;		// transmit enabled
-    TXSTAbits.SYNC = 0;		// asynchronous mode
-    TXSTAbits.BRGH = 1;		// high speed
-    RCSTAbits.SPEN = 1;		// enable serial port (configures RX/DT and TX/CK pins as serial port pins)
-    RCSTAbits.RX9  = 0;		// 8-bit reception
-    RCSTAbits.CREN = USE_BRGH;	// enable receiver
-    BAUDCON1bits.BRG16 = USE_BRG16; // 8-bit baud rate generator
-
-    SPBRG = SBRG_VAL;		// calculated by defines
-    
     TRISCbits.TRISC6 = 0;	// make the TX pin a digital output
     TRISCbits.TRISC7 = 1;	// make the RX pin a digital input
+    TXSTAbits.BRGH = 1;		// high speed
+    BAUDCON1bits.BRG16 = USE_BRG16; // 8-bit baud rate generator
+    SPBRG = SBRG_VAL;		// calculated by defines
     
+    TXSTAbits.SYNC = 0;		// asynchronous mode
+    RCSTAbits.RX9  = 0;		// 8-bit reception
+    RCSTAbits.CREN = 1;		// enable receiver
+
+    TXSTAbits.TX9  = 0;		// 8-bit transmission
+    TXSTAbits.TXEN = 1;		// transmit enabled
+    RCSTAbits.SPEN = 1;		// enable serial port (configures RX/DT and TX/CK pins as serial port pins)
+
     /* don' use interrupts at the moment 
     // interrupts / USART interrupts configuration
     RCONbits.IPEN   = 0; // disable interrupt priority
@@ -32,8 +32,8 @@ void init_usart (void) {
     INTCONbits.PEIE = 1; // enable peripheral interrupts.
     PIE1bits.RCIE   = 1; // enable USART receive interrupt
     PIE1bits.TXIE   = 0; // disable USART TX interrupt
-    */
     PIR1bits.RCIF = 0;
+    */
 }
 
 char putchar(unsigned char c) {
