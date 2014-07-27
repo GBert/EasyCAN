4dd21d99f5b17b742110059bd04d3a9cb4a942b4
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2014-07-27 20:13:14 +0200

    fix CAN timimg values

--- software/firmware/can.h
+++ software/firmware/can.h
@@ -31,67 +31,34 @@ typedef struct CAN_MSG {
     unsigned char Priority;
 };
 
-/* % can-calc-bit-timing -c 64000000 mcp251x
- *
- * Bit timing parameters for mcp251x with 64.000000 MHz ref clock
- * nominal                                 real Bitrt   nom  real SampP
- * Bitrate TQ[ns] PrS PhS1 PhS2 SJW BRP Bitrate Error SampP SampP Error CNF1 CNF2 CNF3
- * 1000000     62   5    6    4   1   4 1000000  0.0% 75.0% 75.0%  0.0% 0x03 0xac 0x03
- *  800000     62   7    8    4   1   4  800000  0.0% 80.0% 80.0%  0.0% 0x03 0xbe 0x03
- *  500000    125   6    7    2   1   8  500000  0.0% 87.5% 87.5%  0.0% 0x07 0xb5 0x01
- *  250000    250   6    7    2   1  16  250000  0.0% 87.5% 87.5%  0.0% 0x0f 0xb5 0x01
- *  125000    500   6    7    2   1  32  125000  0.0% 87.5% 87.5%  0.0% 0x1f 0xb5 0x01
- *  100000    625   6    7    2   1  40  100000  0.0% 87.5% 87.5%  0.0% 0x27 0xb5 0x01
- *   50000   1000   8    8    3   1  64   50000  0.0% 87.5% 85.0%  2.9% 0x3f 0xbf 0x02
- *   20000 ***bitrate not possible***
- *   10000 ***bitrate not possible***
- *
- *   where BRGCONx is CNFx
- */
-
-/* http://www.port.de/cgi-scripts/tq_v1_0.cgi?ctype=pic18&CLK=64&sample_point=87.5 */
+/* http://www.port.de/cgi-scripts/tq_v1_0.cgi?ctype=pic18&CLK=32&sample_point=87.5
+ * there seems to be a BUG in the script (as of 26/7/2014 -> clock must be devided by 2 */
 
 const char BRGCON_64MHZ [][3] = {
-    {0x3f,0xbf,0x02},	/*   10k not possible -> 50k */
-    {0x3f,0xbf,0x02},	/*   20k not possible -> 50k */
-    {0x3f,0xbf,0x02},	/*   50k  */
-    {0x27,0xbc,0x01},	/*  100k */
-    {0x1f,0xbc,0x01},	/*  125k */
-    {0x0f,0xbc,0x01},	/*  250k */
-    {0x07,0xbc,0x01},	/*  500k */
-    {0x04,0xbc,0x01},   /*  800k */
-    {0x03,0xbc,0x01},	/* 1000k */
-};
-
-/* % can-calc-bit-timing -c 16000000 mcp251x
- * Bit timing parameters for mcp251x with 16.000000 MHz ref clock
- * nominal                                 real Bitrt   nom  real SampP
- * Bitrate TQ[ns] PrS PhS1 PhS2 SJW BRP Bitrate Error SampP SampP Error CNF1 CNF2 CNF3
- * 1000000     62   5    6    4   1   1 1000000  0.0% 75.0% 75.0%  0.0% 0x00 0xac 0x03
- *  800000     62   7    8    4   1   1  800000  0.0% 80.0% 80.0%  0.0% 0x00 0xbe 0x03
- *  500000    125   6    7    2   1   2  500000  0.0% 87.5% 87.5%  0.0% 0x01 0xb5 0x01
- *  250000    250   6    7    2   1   4  250000  0.0% 87.5% 87.5%  0.0% 0x03 0xb5 0x01
- *  125000    500   6    7    2   1   8  125000  0.0% 87.5% 87.5%  0.0% 0x07 0xb5 0x01
- *  100000    625   6    7    2   1  10  100000  0.0% 87.5% 87.5%  0.0% 0x09 0xb5 0x01
- *   50000   1250   6    7    2   1  20   50000  0.0% 87.5% 87.5%  0.0% 0x13 0xb5 0x01
- *   20000   3125   6    7    2   1  50   20000  0.0% 87.5% 87.5%  0.0% 0x31 0xb5 0x01
- *   10000   4000   8    8    8   1  64   10000  0.0% 87.5% 68.0% 22.3% 0x3f 0xbf 0x07
- *
- *   where BRGCONx is CNFx
- */
-
-/* http://www.port.de/cgi-scripts/tq_v1_0.cgi?ctype=pic18&CLK=16&sample_point=87.5 */
-
-const char BRGCON_16MHZ [][3] = {
-    {0x3f,0xbf,0x07},	/*   10k sample point 68% */
-    {0x31,0xbc,0x01},	/*   20k */
-    {0x13,0xbc,0x01},	/*   50k */
-    {0x09,0xbc,0x01},	/*  100k */
-    {0x09,0xbc,0x01},	/*  125k */
+    {0x3f,0xbf,0x02},	/*   10k not possible -> 20k */
+    {0x3f,0xbf,0x07},	/*   20k */
+    {0x27,0xbc,0x02},	/*   50k */
+    {0x13,0xbc,0x01},	/*  100k */
+    {0x0f,0xbc,0x01},	/*  125k */
     {0x07,0xbc,0x01},	/*  250k */
     {0x03,0xbc,0x01},	/*  500k */
-    {0x01,0xbc,0x01},	/*  800k */
-    {0x00,0xbc,0x01},	/* 1000k */
+    {0x04,0x84,0x00},   /*  800k */
+    {0x01,0xbc,0x01},	/* 1000k */
+};
+
+/* http://www.port.de/cgi-scripts/tq_v1_0.cgi?ctype=pic18&CLK=8&sample_point=87.5
+ * there seems to be a BUG in the script (as of 26/7/2014 -> clock must be devided by 2 */
+
+const char BRGCON_16MHZ [][3] = {
+    {0x31,0xbf,0x07},	/*   10k */
+    {0x18,0xbc,0x01},	/*   20k */
+    {0x09,0xbc,0x01},	/*   50k */
+    {0x04,0xbc,0x01},	/*  100k */
+    {0x03,0xbc,0x01},	/*  125k */
+    {0x01,0xbc,0x01},	/*  250k */
+    {0x00,0xbc,0x01},	/*  500k */
+    {0x00,0x86,0x00},	/*  800k */
+    {0x00,0x84,0x00},	/* 1000k */
 };
 
 #endif		/* _CAN_H_ */
