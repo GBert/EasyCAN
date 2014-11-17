485a9a91fcbf7bbd4f1d77a3354209e937a72d76
Gerhard Bertelsmann <info@gerhard-bertelsmann.de>
2014-11-17 15:07:01 +0100

    try Darron's approach

---
+++ software/duke/template.asm
@@ -0,0 +1,65 @@
+;------------------------------------------------------------------------------
+;
+; Title:        Demo for The Wellington Boot Loader for PIC18
+;
+; Copyright:    Copyright (c) 2014 The Duke of Welling Town
+;
+;------------------------------------------------------------------------------
+
+;------------------------------------------------------------------------------
+;   This file is part of The Wellington Boot Loader.
+;
+;   The Wellington Boot Loader is free software: you can redistribute it and/or
+;   modify it under the terms of the GNU General Public License as published
+;   by the Free Software Foundation.
+;
+;   The Wellington Boot Loader is distributed in the hope that it will be
+;   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;   GNU General Public License for more details.
+;
+;   You should have received a copy of the GNU General Public License along
+;   with The Wellington Boot Loader. If not, see http://www.gnu.org/licenses/
+;------------------------------------------------------------------------------
+
+                RADIX   DEC
+
+;------------------------------------------------------------------------------
+; Device
+;------------------------------------------------------------------------------
+
+                PROCESSOR 18fXXX
+
+;------------------------------------------------------------------------------
+; Device Constants
+;------------------------------------------------------------------------------
+
+#INCLUDE        "devices.inc"
+ERRORLEVEL      -302
+
+;------------------------------------------------------------------------------
+; MCU Settings
+;------------------------------------------------------------------------------
+
+#DEFINE         CLOCK   0
+
+;------------------------------------------------------------------------------
+; Reset
+;------------------------------------------------------------------------------
+
+                ORG     0
+                GOTO    INIT
+
+;------------------------------------------------------------------------------
+; Init.
+;------------------------------------------------------------------------------
+
+INIT
+                GOTO    INIT
+
+;------------------------------------------------------------------------------
+                END
+;------------------------------------------------------------------------------
+;
+; vim: shiftwidth=4 tabstop=4 softtabstop=4 expandtab
+;
